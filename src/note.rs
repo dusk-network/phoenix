4205ba3764947d7be39d9149dbc03d90a65307ed
zer0 <matteo@dusk.network>
2020-09-21 14:42:37 +0200

    Fix headers
    - Add github workflow to check the headers
    - Update `dusk-pki` to `v0.1.1`
    - Small fix to run on current rust stable

--- src/note.rs
+++ src/note.rs
@@ -1,5 +1,8 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 // Copyright (c) DUSK NETWORK. All rights reserved.
-// Licensed under the MPL 2.0 license. See LICENSE file in the project root for details.
 
 use kelvin::{ByteHash, Content, Sink, Source};
 use std::convert::TryFrom;
@@ -82,7 +85,7 @@ impl std::fmt::Debug for Note {
             .field("pos", &self.pos)
             .field(
                 "encrypted_data",
-                &format_args!("{:?}", &self.encrypted_data),
+                &format_args!("{:?}", &self.encrypted_data[..]),
             )
             .finish()
     }
