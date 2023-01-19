cd6b11211b1b911300572892acb5331cff469ac2
Eduardo Leegwater Simões <eduardo@dusk.network>
2023-01-19 13:33:11 +0100

    Add types to interact with the genesis contracts
    This requires the addition of `dusk-bls12_381-sign` as a dependency, as
    well as helper functions producing messages intended for signing.
    
    Resolves #119

--- src/transaction.rs
+++ src/transaction.rs
@@ -6,6 +6,12 @@
 
 //! Verifier data for the transfer circuits
 
+mod stake;
+pub use stake::*;
+
+mod transfer;
+pub use transfer::*;
+
 use alloc::string::String;
 use alloc::vec::Vec;
 
@@ -14,12 +20,11 @@ use rkyv::{Archive, Deserialize, Serialize};
 
 use dusk_bls12_381::BlsScalar;
 use dusk_bytes::{DeserializableSlice, Error as BytesError, Serializable};
-use dusk_poseidon::cipher::PoseidonCipher;
 
-use crate::{Crossover, Fee, Message, Note};
+use crate::{Crossover, Fee, Note};
 
-const STCO_MESSAGE_SIZE: usize = 7 + 2 * PoseidonCipher::cipher_size();
-const STCT_MESSAGE_SIZE: usize = 5 + PoseidonCipher::cipher_size();
+/// Type alias for the ID of a module.
+pub type ModuleId = [u8; 32];
 
 /// A phoenix transaction.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -46,36 +51,57 @@ pub struct Transaction {
     /// A call to a contract. The `Vec<u8>` must be an `rkyv`ed representation
     /// of the data the contract expects, and the `String` the name of the
     /// function to call.
-    pub call: Option<(BlsScalar, String, Vec<u8>)>,
+    pub call: Option<(ModuleId, String, Vec<u8>)>,
 }
 
 impl Transaction {
-    /// Return input bytes to a hash function for the transaction.
+    /// Return the input bytes to a hash function for the transaction from its
+    /// components.
     #[must_use]
-    pub fn to_hash_input_bytes(&self) -> Vec<u8> {
+    pub fn hash_input_bytes_from_components(
+        nullifiers: &[BlsScalar],
+        outputs: &[Note],
+        anchor: &BlsScalar,
+        fee: &Fee,
+        crossover: &Option<Crossover>,
+        call: &Option<(ModuleId, String, Vec<u8>)>,
+    ) -> Vec<u8> {
         let mut bytes = Vec::new();
 
-        for nullifier in &self.nullifiers {
+        for nullifier in nullifiers {
             bytes.extend(nullifier.to_bytes());
         }
-        for note in &self.outputs {
+        for note in outputs {
             bytes.extend(note.to_bytes());
         }
 
-        bytes.extend(self.anchor.to_bytes());
-        bytes.extend(self.fee.to_bytes());
+        bytes.extend(anchor.to_bytes());
+        bytes.extend(fee.to_bytes());
 
-        if let Some(crossover) = &self.crossover {
+        if let Some(crossover) = crossover {
             bytes.extend(crossover.to_bytes());
         }
 
-        if let Some((module, fn_name, call_data)) = &self.call {
-            bytes.extend(module.to_bytes());
+        if let Some((module, fn_name, call_data)) = call {
+            bytes.extend(module);
             bytes.extend(fn_name.as_bytes());
             bytes.extend(call_data);
         }
 
         bytes
+    }
+
+    /// Return input bytes to a hash function for the transaction.
+    #[must_use]
+    pub fn to_hash_input_bytes(&self) -> Vec<u8> {
+        Self::hash_input_bytes_from_components(
+            &self.nullifiers,
+            &self.outputs,
+            &self.anchor,
+            &self.fee,
+            &self.crossover,
+            &self.call,
+        )
     }
 
     /// Serialize the transaction to a variable length byte buffer.
@@ -113,7 +139,7 @@ impl Transaction {
 
         if let Some((module, fn_name, call_data)) = &self.call {
             bytes.push(1);
-            bytes.extend(module.to_bytes());
+            bytes.extend(module);
 
             let size = fn_name.len() as u64;
             bytes.extend(size.to_bytes());
@@ -159,8 +185,22 @@ impl Transaction {
 
         let has_call = buffer[0] != 0;
         let mut buffer = &buffer[1..];
+
         let call = if has_call {
-            let module = BlsScalar::from_reader(&mut buffer)?;
+            let buffer_len = buffer.len();
+            if buffer.len() < 32 {
+                return Err(BytesError::BadLength {
+                    found: buffer_len,
+                    expected: 32,
+                });
+            }
+
+            let (module_buf, buf) = buffer.split_at(32);
+            buffer = buf;
+
+            let mut module = [0u8; 32];
+            module.copy_from_slice(module_buf);
+
             let fn_name_size = u64::from_reader(&mut buffer)? as usize;
             let fn_name = String::from_utf8(buffer[..fn_name_size].to_vec())
                 .map_err(|_err| BytesError::InvalidData)?;
@@ -202,40 +242,7 @@ impl Transaction {
     }
 
     /// Returns the call of the transaction.
-    pub fn call(&self) -> Option<&(BlsScalar, String, Vec<u8>)> {
+    pub fn call(&self) -> Option<&(ModuleId, String, Vec<u8>)> {
         self.call.as_ref()
     }
-}
-
-/// Signature message used for [`Stct`].
-#[must_use]
-pub fn process_message_stct(
-    crossover: &Crossover,
-    value: u64,
-    module_id: BlsScalar,
-) -> [BlsScalar; STCT_MESSAGE_SIZE] {
-    let mut array = [BlsScalar::default(); STCT_MESSAGE_SIZE];
-    let hash_inputs = crossover.to_hash_inputs();
-    array[..hash_inputs.len()].copy_from_slice(&hash_inputs);
-    array[hash_inputs.len()..].copy_from_slice(&[value.into(), module_id]);
-    array
-}
-
-/// Signature message used for [`Stco`].
-#[must_use]
-pub fn process_message_stco(
-    crossover: &Crossover,
-    message: &Message,
-    module_id: BlsScalar,
-) -> [BlsScalar; STCO_MESSAGE_SIZE] {
-    let mut array = [BlsScalar::default(); STCO_MESSAGE_SIZE];
-    let crossover_inputs = crossover.to_hash_inputs();
-    let message_inputs = message.to_hash_inputs();
-    array[..crossover_inputs.len()].copy_from_slice(&crossover_inputs);
-    array
-        [crossover_inputs.len()..crossover_inputs.len() + message_inputs.len()]
-        .copy_from_slice(&message_inputs);
-    array[crossover_inputs.len() + message_inputs.len()..]
-        .copy_from_slice(&[module_id]);
-    array
 }
